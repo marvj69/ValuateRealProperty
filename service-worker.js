6534719e34a41252a497d90c6865fffeabbd3db2
Mark Heinonen <heinonenmh@gmail.com>
2026-01-26 09:51:46 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v2.02';
+const CACHE_VERSION = 'v2.03';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 const OFFLINE_URL = './offline.html';
