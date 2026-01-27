2d2a6de6e79a1c347d87674e1da0ba8ee9f721c9
Mark Heinonen <heinonenmh@gmail.com>
2026-01-26 19:59:34 -0500

    Update CACHE_VERSION to v2.08

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v2.07';
+const CACHE_VERSION = 'v2.08';
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 const OFFLINE_URL = './offline.html';
